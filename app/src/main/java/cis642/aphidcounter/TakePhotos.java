7311440adb5a17f992f3eabb399d07955e16821b
pruitt93 <pruitt93@ksu.edu>
2014-10-13 13:54:12 -0500

    presprint3

---
+++ app/src/main/java/cis642/aphidcounter/TakePhotos.java
@@ -0,0 +1,72 @@
+package com.example.pruitt93.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.lang.Object;
+import android.view.View;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.content.ContentValues;
+
+/**
+ * Created by JacobLPruitt on 9/29/2014.
+ */
+public class TakePhotos extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.take_photos);
+        Button next = (Button) findViewById(R.id.accessCameraButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), CapturePhoto.class);
+                startActivityForResult(myIntent, 0);
+                /*String fileName = "new-photo-name.jpg";
+                //create parameters for Intent with filename
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, fileName);
+                values.put(MediaStore.Images.Media.DESCRIPTION, "Image capture by camera");
+                //imageUri is the current activity attribute, define and save it for later usage (also in onSaveInstanceState)
+                imageUri = getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                //create new Intent
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);*/
+
+            }
+
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
