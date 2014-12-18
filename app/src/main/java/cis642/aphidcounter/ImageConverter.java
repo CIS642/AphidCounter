553de0f8625caeb12930be8914fbf9b5d430f8ce
japshvincent <japshvincent@gmail.com>
2014-12-18 13:32:04 -0600

    adding updated imageonverter

--- app/src/main/java/cis642/aphidcounter/ImageConverter.java
+++ app/src/main/java/cis642/aphidcounter/ImageConverter.java
@@ -1,25 +1,14 @@
 package cis642.aphidcounter;
-
-
-import android.graphics.Matrix;
-import android.os.Environment;
 import android.util.Log;
-import android.widget.ImageView;
-
-import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.photo.Photo;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import cis642.aphidcounter.manager.FileManager;
 
@@ -27,37 +16,30 @@ import cis642.aphidcounter.manager.FileManager;
  * Created by Staton on 9/25/2014.
  */
 public class ImageConverter {
-
     /**
      * The source image (as a Matrix).
      */
     private Mat source;
-
     /**
      * The final converted image (as a Matrix).
      */
     private Mat convertedImage;
-
     /**
      * Filename of the source image.
      */
     private String sourceFile;
-
     /**
      * Filename of the converted image.
      */
     private String convertedFile;
-
     /**
      * For managing files.
      */
     private FileManager fileManager = new FileManager();
-
-    /**
-     * Number of aphids found on this photo.
-     */
-    //private int aphidCount = 0;
-
+/**
+ * Number of aphids found on this photo.
+ */
+//private int aphidCount = 0;
     /**
      * Constructs a new ImageConverter object.
      */
@@ -65,25 +47,20 @@ public class ImageConverter {
         this.source = new Mat();
         this.convertedImage = new Mat();
     }
-
     public ImageConverter(String sourceFile)
     {
         this.source = new Mat();
         this.convertedImage = new Mat();
         this.sourceFile = sourceFile;
-
         source = Highgui.imread(fileManager.GetPhotosDirectory() + File.separator + sourceFile);
     }
-
     /**
      * Sets the source image matrix.
      * @param source The source image's matrix.
      */
-
     public void setSource(Mat source) {
         this.source = source;
     }
-
     /**
      * Gets the source image matrix.
      * @return THe source image's matrix.
@@ -91,7 +68,6 @@ public class ImageConverter {
     public Mat getSource() {
         return this.source;
     }
-
     /**
      * Gets the converted image's matrix.
      * @return The converted image's matrix.
@@ -99,16 +75,14 @@ public class ImageConverter {
     public Mat getConvertedImage() {
         return this.convertedImage;
     }
-
-    /**
-     * Get the number of aphids found on this photo.
-     * @return
-     */
-    //public int GetAphidCount()
-    //{
-    //    return this.aphidCount;
-    //}
-
+/**
+ * Get the number of aphids found on this photo.
+ * @return
+ */
+//public int GetAphidCount()
+//{
+// return this.aphidCount;
+//}
     /**
      * Gets the name of the converted file.
      * @return
@@ -118,69 +92,150 @@ public class ImageConverter {
         return this.convertedFile;
     }
 
-    /**
-     * Algorithm for converting the original color image to a black and white binary image.
-     */
-   /* public Mat convertToGrayScale(Mat source){
-        Mat grayScaleImage = new Mat();
-        Log.i("Process Trace","begining grayscale conversion");
-        Imgproc.cvtColor(source, grayScaleImage, Imgproc.COLOR_RGB2GRAY);
-        Log.i("Process Trace","done with grayscale conversion");
-        return grayScaleImage;
-    }*/
-
-
-    private final double bgStrelConst_width = 0.02;
-    private final double bgStrelConst_height = 0.02;
-
     public boolean ConvertImage() {
         boolean success = false;
-        //Getting image variables to be used with respect to image size used.
-        int width = source.width();
-        int height = source.height();
-        int bgStrel_width = (int) Math.round(width * bgStrelConst_width);
-        int bgStrel_height = (int) Math.round(height * bgStrelConst_height);
-
-
-        //converts the image to grayscale
-        Log.i("Process Trace", "begining grayscale conversion");
-        Imgproc.cvtColor(source, convertedImage, Imgproc.COLOR_RGB2GRAY);
-        Log.i("Process Trace","done with grayscale conversion");
-
-
-        //adjust the image intensity/contrast
-        Log.i("Process Trace","begining image intensity/contrast adjustment");
-        convertedImage.convertTo(convertedImage, -1, 1.0, -100.0);
-        Log.i("Process Trace","done with image intensity/contrast adjustment");
-        //Removing Background
-        //Creating background Strel
-        Mat imageBg = convertedImage.clone();
-        Imgproc.medianBlur(imageBg, imageBg, 21);//blur out aphids in bg
-        //Imgproc.threshold(imageBg,imageBg,90,110,Imgproc.THRESH_BINARY);
-        // Imgproc.erode(imageBg,imageBg,Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(bgStrel_width,bgStrel_height)));
-
-
-        Core.absdiff(convertedImage, imageBg, convertedImage);
-        //dont use equalize find alternative for fixing contrast
-
-        //convertedImage.convertTo(convertedImage,-1,1.0, -100);
-
-        Mat round = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_ELLIPSE, new Size(4,4));
-        Imgproc.dilate(convertedImage,convertedImage,round);
-
+        int width, height;
+        double resaled_widthFactor = 1.0;
+        double rescaled_heightFactor = 1.0;
+        Mat resizedImage;
+        width = source.width();
+        height = source.height();
+        resizedImage = new Mat();
+        if(width > 1200 || height > 1200){
+            Imgproc.resize(source,resizedImage,new Size(1000, 850), 0, 0,Imgproc.INTER_NEAREST);
+            resaled_widthFactor = resizedImage.width()/(source.width()*1.0);
+            rescaled_heightFactor = resizedImage.height()/(source.height() * 1.0);
+            width = (int)(width * resaled_widthFactor);
+            height = (int) (height * rescaled_heightFactor);
+            source = resizedImage;
+        }
+        Mat grayScaled = new Mat();
+        grayScaled = grayScalConversion(source);
+        Mat J4 = new Mat();
+        J4 = imadjust(grayScaled);
+        Mat background = new Mat();
+        int strelSize = (int)(450 * resaled_widthFactor);
+        Mat strel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(strelSize,strelSize));
+        background = imopen(grayScaled,strel);
+        Mat I2 = new Mat();
+        Core.absdiff(J4,background,I2);
+        Log.i("IMGocnv.main","removed bg");
+        strelSize = (int)(210 * resaled_widthFactor);
+        Mat octaStrel = octagonStrel(strelSize);
+        Mat I3 = new Mat();
+        I3 = imtophat(I2, octaStrel);
+        Mat I4 = new Mat();
+        I4 = imfill(I3);
+        Mat M1 = new Mat();
+        Imgproc.medianBlur(I4,M1,3);
+        Mat I5 = new Mat();
+        I5 = imfill(M1);
+        Mat I6 = new Mat();
+        strelSize = (int)(45 * resaled_widthFactor);
+        strel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,new Size(strelSize,strelSize));
+        I6 = imtophat(I5, strel);
+        Mat I7 = new Mat();
+        I7 = imfill(I6);
+        Mat M2 = new Mat();
+        Imgproc.medianBlur(I7,M2,3);
+        Mat I8 = new Mat();
+        I8 = imfill(M2);
+        Mat I9 = new Mat();
+        strelSize = (int)(10 * resaled_widthFactor);
+        Log.i("STRELSIZE:", String.valueOf(strelSize));
+        if(strelSize < 3){
+            strelSize = 5;
+        }
+        strel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_ELLIPSE, new Size(strelSize,strelSize));
+        I9 = imtophat(I8,strel);
+        Mat M3 = new Mat();
+        M3 = imfill(I9);
+        Mat I10 = new Mat();
+        strelSize = (int)(25 * resaled_widthFactor);
+        strel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_ELLIPSE, new Size(strelSize,strelSize));
+        I10 = imtophat(M3,strel);
+        Mat I11 = new Mat();
+        I11 = imfill(I10);
+        Mat I12 = new Mat();
+        I12 = imadjust(I11);
+        strel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_ELLIPSE, new Size(4,4));
+        Imgproc.dilate(I12.clone(),I12,strel);
+        I12 = imadjust(I12.clone());
+        Mat heirarchy = new Mat();
+        convertedImage = findEdges(I12);
         convertedFile = "c_" + sourceFile;
-
         String convertedImgFile = fileManager.GetConvertedPhotosDirectory() + File.separator + convertedFile;
 
         try {
             Highgui.imwrite(convertedImgFile, convertedImage);
-            //aphidCount = 0;
-
+                //aphidCount = 0;
             success = true;
         } catch (Exception ex) { ex.printStackTrace(); }
-
         return success;
     }
-
-
+    public static Mat grayScalConversion(Mat src){
+        Mat grayScaled = new Mat();
+        Imgproc.cvtColor(src,grayScaled,Imgproc.COLOR_RGB2GRAY);
+        return grayScaled;
+    }
+    public static Mat imadjust(Mat src){
+        Mat imadjusted = src.clone();
+        //Imgproc.equalizeHist(src,imadjusted);
+        imadjusted.convertTo(src,-1, 1.5, -100.0);
+        return imadjusted;
+    }
+    public static Mat imopen(Mat src, Mat strel){
+        Mat dst = new Mat();
+        Imgproc.erode(src,dst,strel);
+        Log.i("IMGconv.imopen","eroded");
+        Mat dst2 = new Mat();
+        Imgproc.dilate(dst,dst2,strel);
+        Log.i("IMGconv.imopen","dilated");
+        return dst2;
+    }
+    public static Mat imtophat(Mat src,Mat strel){
+        Mat topHatted = new Mat();
+        Mat imopened = new Mat();
+        imopened = imopen(src,strel);
+        Core.absdiff(src,imopened,topHatted);
+        return topHatted;
+    }
+    public static Mat imfill(Mat src){
+        Mat floodMask = new Mat();
+        floodMask = src.clone();
+        Imgproc.Canny(src, floodMask, 100, 255);
+        Imgproc.copyMakeBorder(floodMask, floodMask, 1, 1, 1, 1, Imgproc.BORDER_REPLICATE);
+        Mat filled = src.clone();
+        Imgproc.floodFill(filled,floodMask,new Point(1,1), new Scalar(255));
+        return filled;
+    }
+    public static Mat findEdges(Mat src){
+        Mat mask = new Mat();
+        Imgproc.Canny(src,mask,150,200);
+        return mask;
+    }
+    public static Mat octagonStrel(int size){
+        Mat octaStrel = new Mat();
+        int dim = 1 + (size *2);
+        Mat strel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(dim,dim));
+        octaStrel = rotate(strel);
+        return octaStrel;
+    }
+    private static Mat rotate(Mat src){
+        Point pt = new Point(src.width()/2,src.height()/2);
+        Mat r = new Mat();
+        r = Imgproc.getRotationMatrix2D(pt,45.0,1.0);
+        Mat rotatedImage = new Mat();
+        Imgproc.warpAffine(src,rotatedImage,r,new Size(src.width(),src.height()));
+        return rotatedImage;
+    }
+    private static void displayMat(Mat mat){
+        for(int i = 0 ; i < mat.width(); i ++){
+            String line = "";
+            for(int j = 0 ; j < mat.height() ; j ++){
+                line += String.valueOf((int)mat.get(i,j)[0]) + " ";
+            }
+            Log.i("",line);
+        }
+    }
 }
